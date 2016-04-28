e356f349a1f6f48de6097d01b12d39b5cf733306
shima15 <s15187so@sfc.keio.ac.jp>
2016-04-28 12:39:19 +0900

    関数exorの定義

--- js/logic.js
+++ js/logic.js
@@ -7,7 +7,14 @@ function imply(a, b){
 
 function nad(a, b){
     var result;
-    result = !(a && b)
+    result = !(a && b);
+    
+    return result;
+}
+
+function exor(a, b){
+    var result;
+    result = (a && !b) || (!a && b);
     
     return result;
 }
